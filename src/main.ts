f8e10cad3cfe420417a298a08d4178951c729eb0
João Neves <119359716+JoaoNevez@users.noreply.github.com>
2025-08-27 13:10:47 -0300

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const app = document.getElementById("app")!;
+
+app.innerHTML = `
+  <p>🔐 Bem-vindo ao Cofre de Notas em TypeScript!</p>
+  <p>Aqui você poderá guardar suas notas de forma criptografada.</p>
+`;
